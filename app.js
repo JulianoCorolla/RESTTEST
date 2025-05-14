3f5e2b351f1d33fd8abb8b23a39db16dfe083999
ALUNO C24 <alunoc24@unifor.local>
2025-05-14 19:32:36 -0300

    issoai

--- app.js
+++ app.js
@@ -1,26 +1,109 @@
-var express = require('express');
-var app = express();
-var fs = require("fs");
+//import express from 'express';
+const express = require('express');
 
-app.get('/listUsers', function (req, res) {
-   fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
-      console.log( data );
-      res.end( data );
-   });
-})
+//import createClient from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
+//import {createClient} from '@supabase/supabase-js'
+const supabaseClient = require('@supabase/supabase-js');
 
-app.get('/listUser/:id', function (req, res) {
-    // First read existing users.
-    fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
-       var users = JSON.parse( data );
-       var user = users["user" + req.params.id] 
-       console.log( user );
-       res.end( JSON.stringify(user));
-    });
- })
+//import morgan from 'morgan';
+const morgan = require('morgan');
 
-var server = app.listen(8081, function () {
-   var host = server.address().address
-   var port = server.address().port
-   console.log("Example app listening at http://%s:%s", host, port)
-})
+//import bodyParser from "body-parser";
+const bodyParser = require('body-parser');
+
+//import { createClient } from "https://cdn.skypack.dev/@supabase/supabase-js";
+
+const app = express();
+
+const cors=require("cors");
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
+
+app.use(cors(corsOptions)) // Use this after the variable declaration
+
+
+// using morgan for logs
+app.use(morgan('combined'));
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+const supabase = 
+    supabaseClient.createClient('https://nduwikxngvfhmjzujlky.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5kdXdpa3huZ3ZmaG1qenVqbGt5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDcwODkwMzksImV4cCI6MjA2MjY2NTAzOX0.rYXJbcUJB_Y6Sf6p9JZUxXQ1IIYYF3CMzCwB0gTnN3g')
+
+app.get('/products', async (req, res) => {
+    const {data, error} = await supabase
+        .from('products')
+        .select()
+    res.send(data);
+    console.log(`lists all products${data}`);
+});
+
+app.get('/products/:id', async (req, res) => {
+    console.log("id = " + req.params.id);
+    const {data, error} = await supabase
+        .from('products')
+        .select()
+        .eq('id', req.params.id)
+    res.send(data);
+
+    console.log("retorno "+ data);
+});
+
+app.post('/products', async (req, res) => {
+    const {error} = await supabase
+        .from('products')
+        .insert({
+            name: req.body.name,
+            description: req.body.description,
+            price: req.body.price,
+        })
+    if (error) {
+        res.send(error);
+    }
+    res.send("created!!");
+    console.log("retorno "+ req.body.name);
+    console.log("retorno "+ req.body.description);
+    console.log("retorno "+ req.body.price);
+
+});
+
+app.put('/products/:id', async (req, res) => {
+    const {error} = await supabase
+        .from('products')
+        .update({
+            name: req.body.name,
+            description: req.body.description,
+            price: req.body.price
+        })
+        .eq('id', req.params.id)
+    if (error) {
+        res.send(error);
+    }
+    res.send("updated!!");
+});
+
+app.delete('/products/:id', async (req, res) => {
+    console.log("delete: " + req.params.id);
+    const {error} = await supabase
+        .from('products')
+        .delete()
+        .eq('id', req.params.id)
+    if (error) {
+        res.send(error);
+    }
+    res.send("deleted!!")
+    console.log("delete: " + req.params.id);
+
+});
+
+app.get('/', (req, res) => {
+    res.send("Hello I am working my friend Supabase <3");
+});
+
+app.listen(3000, () => {
+    console.log(`> Ready on http://localhost:3000`);
+});
